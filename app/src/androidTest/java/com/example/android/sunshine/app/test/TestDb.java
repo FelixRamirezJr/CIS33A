39244165f7a8800fca54cffe7c3775216c119881
Felix <felix@mail.sfsu.edu>
2015-06-02 21:03:06 -0700

    This should be the SQL stuff done

--- app/src/androidTest/java/com/example/android/sunshine/app/test/TestDb.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/test/TestDb.java
@@ -7,6 +7,7 @@ package com.example.android.sunshine.app.test;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
@@ -16,6 +17,8 @@ import com.example.optimusprime.cis53hw2_03495.WeatherDbHelper;
 
 import java.util.Map;
 import java.util.Set;
+
+import static android.database.DatabaseUtils.dumpCursorToString;
 
 public class TestDb extends AndroidTestCase
 {
@@ -28,7 +31,8 @@ public class TestDb extends AndroidTestCase
     static final String INSERT_LOCATION = "99705"; // Going to the Same Location.
     static final String INSERT_DATE = "20151306";
 
-    public void testCreateDb() throws Throwable {
+    public void testCreateDb() throws Throwable
+    {
         Log.d(LOG_TAG, "HMMMM ThIS IS CREATING THE DATABASE");
         mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
         SQLiteDatabase db = new WeatherDbHelper(
@@ -50,20 +54,22 @@ public class TestDb extends AndroidTestCase
         long locationRowId;
         locationRowId = db.insert(LocationEntry.TABLE_NAME, null, testValues);
 
-        Log.d(LOG_TAG,"This is the size of the testValues" + testValues.size());
-        Log.d(LOG_TAG, "THIS IS JUST SOME RANDOM MESSAGE TO SEE WHERE IT COMES FROM ");
-
-        // Verify we got a row back.
-        Log.d("MyApp","I AM CHECKING IF I AM GETTING SOMETHING HERE OR WTF...");
-
         Log.d("MyApp","This should be the Size of the Data" + testValues.size());
-        Log.d("MyApp","This is the locationRowID" + locationRowId);
+
 
         assertTrue(locationRowId != -1);
         Log.d(LOG_TAG, "New row id: " + locationRowId);
 
         // Data's inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
         // the round trip.
+
+        String [] columns = {
+                LocationEntry._ID,
+                LocationEntry.COLUMN_LOCATION_SETTING,
+                LocationEntry.COLUMN_CITY_NAME,
+                LocationEntry.COLUMN_COORD_LAT,
+                LocationEntry.COLUMN_COORD_LONG
+        };
 
         // A cursor is your primary interface to the query results.
         Cursor cursor = db.query(
@@ -95,9 +101,73 @@ public class TestDb extends AndroidTestCase
                 null  // sort order
         );
 
-        validateCursor(weatherCursor, weatherValues);
 
+        // This Will print the Query Data.
+        dumpCursorToString(weatherCursor);
+        Log.d("MyApp", "This Will Query The Data: " + weatherCursor.toString());
+        Log.d("MyApp", "The is the Size of the Query: " + weatherCursor.getColumnCount());
+
+        Log.d("OUTPUT", "Should Have Printed Stuff");
+
+
+        validateCursor(weatherCursor, weatherValues);
         dbHelper.close();
+
+       // insertAgain();
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    /////////////////////// INSERT INTO THE DATABSE AGAIN! //////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    void insertAgain ()
+    {
+       // db = openOrCreateDatabase( "weather.db"        , SQLiteDatabase.CREATE_IF_NECESSARY        , null          );
+
+        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues NewValues = createValuesForInsertaion(); // Calls A different Method To Insert.
+
+        long locationRowId;
+        locationRowId = db.insert(LocationEntry.TABLE_NAME, null, NewValues);
+
+        assertTrue(locationRowId != -1);
+
+        // Creating The Cursors...
+        Cursor cursor = db.query(
+                LocationEntry.TABLE_NAME,  // Table to Query
+                null, // all columns
+                null, // Columns for the "where" clause
+                null, // Values for the "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null // sort order
+        );
+
+        validateCursor(cursor, NewValues); // Validate It
+
+        //////////////////// END OF THE FIRST INSERTION /////////////////////
+
+        // Fantastic.  Now that we have a location, add some weather!
+        ContentValues NewWeatherVals = insertWeatherValues(locationRowId); // Inserting New Weather Values..
+
+        long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, NewWeatherVals);
+        assertTrue(weatherRowId != -1);
+
+        // A cursor is your primary interface to the query results.
+        Cursor weatherCursor = db.query(
+                WeatherEntry.TABLE_NAME,  // Table to Query
+                null, // leaving "columns" null just returns all the columns.
+                null, // cols for "where" clause
+                null, // values for "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null  // sort order
+        );
+
+        validateCursor(weatherCursor, NewWeatherVals);
+        dbHelper.close();
+
     }
 
     static ContentValues createWeatherValues(long locationRowId) {
@@ -116,6 +186,7 @@ public class TestDb extends AndroidTestCase
         return weatherValues;
     }
 
+
     static ContentValues createNorthPoleLocationValues() {
         // Create a new map of values, where column names are the keys
         ContentValues testValues = new ContentValues();
@@ -127,14 +198,38 @@ public class TestDb extends AndroidTestCase
         return testValues;
     }
 
+    //////////////////////////////////////////////////////////////
+    /////////////////// Used For Insertion //////////////////////
+    /////////////////////////////////////////////////////////////
+
     static ContentValues  createValuesForInsertaion ()
     {
         ContentValues insertValues = new ContentValues();
         insertValues.put(LocationEntry.COLUMN_LOCATION_SETTING,INSERT_LOCATION); // Placing The insert Location
+        insertValues.put(LocationEntry.COLUMN_CITY_NAME, "North Pole");
+        insertValues.put(LocationEntry.COLUMN_COORD_LAT, 64.7488);
+        insertValues.put(LocationEntry.COLUMN_COORD_LONG, -147.353);
 
         return insertValues;
 
     }
+    static ContentValues insertWeatherValues (long locationRowId)
+    {
+
+        ContentValues weatherValues = new ContentValues();
+        weatherValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+        weatherValues.put(WeatherEntry.COLUMN_DATETEXT, INSERT_DATE);
+        weatherValues.put(WeatherEntry.COLUMN_DEGREES, 100);
+        weatherValues.put(WeatherEntry.COLUMN_HUMIDITY, 5.6);
+        weatherValues.put(WeatherEntry.COLUMN_PRESSURE, 6.6);
+        weatherValues.put(WeatherEntry.COLUMN_MAX_TEMP, 66);
+        weatherValues.put(WeatherEntry.COLUMN_MIN_TEMP, 65);
+        weatherValues.put(WeatherEntry.COLUMN_SHORT_DESC, "An Apocalypse");
+        weatherValues.put(WeatherEntry.COLUMN_WIND_SPEED, 100);
+        weatherValues.put(WeatherEntry.COLUMN_WEATHER_ID, 322); // Changed From 321
+        return weatherValues;
+    }
+    /////////////////////////////// End of Insertion Methods //////////////////////
 
     static void validateCursor(Cursor valueCursor, ContentValues expectedValues) {
 
